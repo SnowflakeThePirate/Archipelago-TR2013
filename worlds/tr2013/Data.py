4e11ebe535586284e818e29c68d10a2ed5e77b5a
Snowflake The Pirate <snowflakethepirate@gmail.com>
2026-06-30 12:03:03 -0600

    testing

--- worlds/tr2013/Data.py
+++ worlds/tr2013/Data.py
@@ -1,5 +1,19 @@
 import os
 import json
+from BaseClasses import Item, Location
+
+class TR2013Item(Item):
+    name: str
+    id: int
+    game_id: str
+    type: str
+    tier: str
+
+class TR2013Location(Location):
+    name: str
+    id: int
+    item_id: str
+    region: str
 
 class Data:
     item_table = []
@@ -10,8 +24,8 @@ class Data:
     def load_data(cls):
         data_file_path = os.path.join(os.path.dirname(__file__), 'items.json')
         with open(data_file_path, 'r') as f:
-            Data.item_table = json.load(f)
+            Data.item_table = [TR2013Item(**item) for item in json.load(f)]
 
         location_file_path = os.path.join(os.path.dirname(__file__), 'locations.json')
         with open(location_file_path, 'r') as f:
-            Data.location_table = json.load(f)
+            Data.location_table = [TR2013Location(**location) for location in json.load(f)]
--- worlds/tr2013/__init__.py
+++ worlds/tr2013/__init__.py
@@ -1,10 +1,16 @@
 from worlds.AutoWorld import World
 
-from .Data import Data
+from .Data import Data, TR2013Item, TR2013Location
+from BaseClasses import ItemClassification, Item, Location, Region, CollectionState, LocationProgressType
+from typing import Optional
 
 Data.load_data()
 
 def run_client():
+    # hook into tomb raider
+    # watch memory for flag flips
+    # when certain flag flips grant items
+    # Case 1, pick up Document: flag should show in client
     pass
 
 class TR2013World(World):
@@ -15,10 +21,27 @@ class TR2013World(World):
     """
     game: str = "Tomb Raider (2013)"
 
-    item_name_to_id = { item['name']: item['id'] for item in Data.item_table }
-    location_name_to_id = { location['name']: location['id'] for location in Data.location_table }
+    item_name_to_id = { item.name: item.id for item in Data.item_table }
+    location_name_to_id = { location.name: location.id for location in Data.location_table }
     # item_name_groups = { key: set(values) for key, values in Data.item_name_groups.items() }
 
 
     def __init__(self, world, player):
         super(TR2013World, self).__init__(world, player)
+
+    def create_items(self) -> None:
+        item_pool = [self.create_item(item) for item in Data.item_table]
+        while len(item_pool) < len(Data.location_table):
+            item_pool.append(self.create_item(None))
+        self.multiworld.itempool += item_pool
+
+    def create_item(self, item: Optional[TR2013Item]) -> Item:
+        if not item:
+            name = "Junk"
+            item_classification = ItemClassification.filler
+        else:
+            if item.type in ['weapon', 'gear']:
+                item_classification = ItemClassification.progression
+            else:
+                item_classification = ItemClassification.useful
+        return Item(name, item_classification, self.item_name_to_id[name], self.player)
